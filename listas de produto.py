7da43a62ef438a7bbbecfd1ecfe93d60cdf7fdaa
LeoTAlves <ltalves89@gmail.com>
2026-05-06 20:33:02 -0300

    listas de produto

---
+++ listas de produto.py
@@ -0,0 +1,9 @@
+produto=["arroz", "feijão", "macarrão", "leite", "pão"]
+
+print("lista completa,", produto)
+
+print("primeiro produto,", produto[0])
+
+print("último produto,", produto[4])
+
+print("total produtos na lista,", len(produto))
