23ebd921ac868efe76563d5427ace4d8e7ebd30e
LeoTAlves <ltalves89@gmail.com>
2026-05-06 21:13:45 -0300

    listas produtos e preços

--- listas de produto.py
+++ listas de produto.py
@@ -1,4 +1,5 @@
 produto=["arroz", "feijão", "macarrão", "leite", "pão"]
+preços=[20.00 , 8.00 , 2.00 , 4.50 , 8.00]
 
 print("lista completa,", produto)
 
@@ -7,3 +8,22 @@ print("primeiro produto,", produto[0])
 print("último produto,", produto[4])
 
 print("total produtos na lista,", len(produto))
+
+#Para exibir:
+print(f"O produto {produto[0]} custa R$ {preços[0]}")
+
+#Para remover o ultimo produto e o preço também:
+produto.remove(produto[-1])
+preços.remove(preços[-1])
+
+#Para somar o preço de todos os produtos:
+total=sum(preços)
+print(f"O total deu R${total}")
+
+# Logica condicional if/else para descontos:
+if total <30:
+    exit()
+else:
+    desconto = 0.95
+    total = total * desconto
+    print(f"O total com desconto é de R$:{total}.")
