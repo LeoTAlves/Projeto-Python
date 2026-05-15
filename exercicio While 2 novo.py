4e1cc298fea07aead05bd059c435b3f2cac3429f
LeoTAlves <ltalves89@gmail.com>
2026-05-14 21:59:30 -0300

    Exercicio while com listas

--- exercicio While 2 novo.py
+++ exercicio While 2 novo.py
@@ -1,7 +1,9 @@
 contador = 0
+numeros = []
 numero = -1
 while numero != 0:
     numero = int(input("Digite um número (0 para sair): "))
     if numero != 0:
         contador = contador + 1
+        numero.append(numeros)
 print(f"Quantidade de números digitados: {contador}")
