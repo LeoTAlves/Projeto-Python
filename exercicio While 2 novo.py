fe7fb89588b23bcbcfe5b77687a18d453da8e15b
LeoTAlves <ltalves89@gmail.com>
2026-05-14 21:47:05 -0300

    sem listas

---
+++ exercicio While 2 novo.py
@@ -0,0 +1,7 @@
+contador = 0
+numero = -1
+while numero != 0:
+    numero = int(input("Digite um número (0 para sair): "))
+    if numero != 0:
+        contador = contador + 1
+print(f"Quantidade de números digitados: {contador}")
