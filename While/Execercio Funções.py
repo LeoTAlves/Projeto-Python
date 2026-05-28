66b5dcf0db70cee2d033107500d1cbbf405ecd0d
LeoTAlves <ltalves89@gmail.com>
2026-05-28 20:17:39 -0300

    envio

---
+++ While/Execercio Funções.py
@@ -0,0 +1,62 @@
+def somar(n1 , n2):
+    total = n1 + n2
+    print(f" {n1} + {n2} = {total}")
+    return total
+
+def subtrair(n1 , n2):
+    total = n1 - n2
+    print(f" {n1} - {n2} = {total}")
+    return total
+
+def multiplicar(n1 , n2):
+    total = n1 * n2
+    print(f" {n1} * {n2} = {total}")
+    return total
+
+def dividir(n1 , n2):
+    total = n1 / n2
+    if n2 == 0: print(" Não tem como dividir por ZERO")
+    else:
+        total = n1 / n2
+        print(f" {n1} / {n2} = {total} ")
+    return total
+
+while True:
+    menu = """
+    === CALCULADORA ===
+    
+    1 - Soma (+)
+    2 - Subtração (-)
+    3 - Multiplicação (x)
+    4 - Divisão (/)
+    0 - Sair
+    """
+    print(menu)
+
+    escolha = input("digite a opção: ")
+
+    if escolha == "0" : break
+
+    if escolha not in ["1", "2", "3", "4", "0"]:
+        print("Opção invalida! Tente novamente")
+        continue
+
+    n1 = int(input("digite um número: "))
+    n2 = int(input("digite outro número: "))
+    
+    if escolha == "1":
+        total = somar (n1 , n2)
+    
+    elif escolha == "2":
+        total = subtrair (n1 , n2)
+    
+    elif escolha == "3":
+        total = multiplicar (n1 , n2)
+    
+    elif escolha == "4":
+        total = dividir (n1, n2)
+        
+    else:
+        print("opção invalida")
+    
+
