3184e6703cbd25c239398c46eb53c2759108cd9e
LeoTAlves <ltalves89@gmail.com>
2026-05-28 21:14:52 -0300

    Def.

--- While/Execercio Funções.py
+++ While/Execercio Funções.py
@@ -1,25 +1,19 @@
 def somar(n1 , n2):
     total = n1 + n2
     print(f" {n1} + {n2} = {total}")
-    return total
-
+    
 def subtrair(n1 , n2):
     total = n1 - n2
     print(f" {n1} - {n2} = {total}")
-    return total
-
+    
 def multiplicar(n1 , n2):
     total = n1 * n2
     print(f" {n1} * {n2} = {total}")
-    return total
-
+    
 def dividir(n1 , n2):
-    total = n1 / n2
-    if n2 == 0: print(" Não tem como dividir por ZERO")
-    else:
-        total = n1 / n2
-        print(f" {n1} / {n2} = {total} ")
-    return total
+    if n2 == 0:print(" Não tem como dividir por ZERO")
+    else: print(f" {n1} / {n2} = {round(n1 / n2, 2)}")
+    
 
 while True:
     menu = """
@@ -44,19 +38,10 @@ while True:
     n1 = int(input("digite um número: "))
     n2 = int(input("digite outro número: "))
     
-    if escolha == "1":
-        total = somar (n1 , n2)
-    
-    elif escolha == "2":
-        total = subtrair (n1 , n2)
-    
-    elif escolha == "3":
-        total = multiplicar (n1 , n2)
-    
-    elif escolha == "4":
-        total = dividir (n1, n2)
-        
-    else:
-        print("opção invalida")
+    if escolha == "1": somar (n1 , n2)    
+    elif escolha == "2": subtrair (n1 , n2)    
+    elif escolha == "3": multiplicar (n1 , n2)    
+    elif escolha == "4": dividir (n1, n2)
+    else:print("opção invalida")
     
 
