c76908bd3b6a44fa759ccd00a49f46afe47e7326
LeoTAlves <ltalves89@gmail.com>
2026-06-08 20:04:37 -0300

    conta corrigida

---
+++ Conta Bancaria.py
@@ -0,0 +1,48 @@
+def exibir_menu():
+    print("\n====================")
+    print("CONTA BANCÁRIA")
+    print("1 - Depositar")
+    print("2 - Sacar")
+    print("3 - Ver Saldo")
+    print("0 - Sair")
+    print("====================")
+
+def depositar(saldo):
+    valor = float(input("Digite um valor para deposito: "))
+    saldo = saldo + valor
+    print(f"Depósito de {valor}realizado com sucesso!")
+    return saldo
+
+def sacar(saldo):
+    saque = float(input("Digite um valor para saque: "))
+    if saque > saldo:
+        print("Saldo insuficiente!")
+        return saldo
+    
+    saldo = saldo - saque
+    print(f"Saque de {saque}srealizado com sucesso!")
+    return saldo 
+
+def ver_saldo(saldo):
+    print(f"Saldo atual: R$ {saldo:.2f}")
+
+saldo = 0.0
+while True:
+    exibir_menu()
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "0":
+        print("Encerrando o programa. Até mais!")
+        break
+        
+    elif opcao == "1":
+        saldo = depositar(saldo)
+        
+    elif opcao == "2":
+        saldo = sacar(saldo)
+        
+    elif opcao == "3":
+        ver_saldo(saldo)
+        
+    else:
+        print("Opção inválida! Tente novamente.")
